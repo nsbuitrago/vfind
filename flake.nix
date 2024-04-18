58786e4ae6f111d6298d5ecd99a186a52d12de70
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-04-18 01:13:04 -0500

    add: nix flake for builds

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "vFind devenv";
+
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    nixpkgs-python.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  packages = with pkgs; [
+                    cmake
+                    libclang
+                    libllvm
+                    pkg-config
+                    maturin
+                    # python3 packages
+                    pkgs.python312Packages.pytest
+                    pkgs.python312Packages.polars
+                  ];
+
+                  languages.python = {
+                    enable = true;
+                    version = "3.12";
+                    manylinux.enable = true;
+                  };
+
+                  languages.rust.enable = true;
+                  languages.rust.channel = "nightly";
+                  enterShell = ''
+                    export LIBCLANG_PATH="$DEVENV_PROFILE/lib"
+                  '';
+
+                }
+              ];
+            };
+          });
+    };
+}
