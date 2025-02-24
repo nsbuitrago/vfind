c4a649436218aff12f25c75b088ea33a612566e3
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2025-02-24 10:58:56 -0600

    test: simplify with pytest

--- tests/test_vfind.py
+++ tests/test_vfind.py
@@ -1,24 +1,12 @@
 from vfind import find_variants
 import polars as pl
-import logging
-import unittest
 
+def test_vfind():
+    variants = find_variants(
+        "test_data/toy.fq.gz",
+        ("GGGCCCAGCCGGCCGGAT", "CCGGAGGCGGAGGTTCAG"),
+        show_progress=False,
+    )
+    ground_truth = pl.read_csv("test_data/ground_truth.csv")
 
-class TestFinder(unittest.TestCase):
-    """
-    Test find_variants from python
-    """
-
-    def test_vfind(self):
-        variants = find_variants(
-            "test_data/toy.fq.gz",
-            ("GGGCCCAGCCGGCCGGAT", "CCGGAGGCGGAGGTTCAG"),
-            show_progress=False,
-        )
-        ground_truth = pl.read_csv("test_data/ground_truth.csv")
-
-        assert variants.equals(ground_truth)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert variants.equals(ground_truth)
