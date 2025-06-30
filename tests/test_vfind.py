b3e1df3590f14250c583571efa3d38cc8f0515ff
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2025-06-30 14:01:47 -0500

    update(tests): sanity check test

--- tests/test_vfind.py
+++ tests/test_vfind.py
@@ -1,13 +1,60 @@
 from vfind import find_variants
 import polars as pl
+import os.path as path
 
 
-def test_vfind():
-    variants = find_variants(
-        "tests/test_data/toy.fq.gz",
+TEST_DATA_PATH = path.join("tests", "test_data")
+
+MAGICAL_DF = pl.DataFrame({
+    "sequence": ["MAGICAL"],
+    "count": [4],
+})
+
+LIIGAND_DF = pl.DataFrame({
+    "sequence": ["LIIGAND"],
+    "count": [4],
+})
+
+def _test_recovery(adapters: tuple[str, str], fq_path: str, expected_df: pl.DataFrame) -> None:
+    """
+    Helper function for testing variant recovery
+
+    Parameters
+    ----------
+
+    adapters (tuple(str, str)): Prefix and suffix adapters to search.
+    fq_path (str): Path to input fastq file.
+    csv_path (str): Path to ground truth csv file.
+    """
+    variants = find_variants(fq_path, adapters, show_progress=False)
+    assert variants.equals(expected_df)
+
+
+def test_long_adapter_recovery():
+    _test_recovery(
         ("GGGCCCAGCCGGCCGGAT", "CCGGAGGCGGAGGTTCAG"),
-        show_progress=False,
+        path.join(TEST_DATA_PATH, "toy_18bp_barcode.fq.gz"),
+        #path.join(TEST_DATA_PATH, "toy_ground_truth.csv")
+        MAGICAL_DF
     )
-    ground_truth = pl.read_csv("tests/test_data/ground_truth.csv")
 
-    assert variants.equals(ground_truth)
+def test_short_adapter_recovery():
+    _test_recovery(
+        ("GATCATG", "GAACTGC"),
+        path.join(TEST_DATA_PATH, "toy_7bp_barcode.fq.gz"),
+        MAGICAL_DF
+    )
+
+
+def test_magical_demultiplex():
+    _test_recovery(
+        ("GATCATG", "GAACTGC"),
+        path.join(TEST_DATA_PATH, "demultiplex.fq.gz"),
+        MAGICAL_DF
+    )
+
+    _test_recovery(
+        ("GATCATG", "ACCAGGT"),
+        path.join(TEST_DATA_PATH, "demultiplex.fq.gz"),
+        LIIGAND_DF
+    )
